d85696dc05fc417b4de6b9c05702f8f50b08af1f
Mirco Lindenau <mirco.lindenau@gmx.de>
2022-12-27 15:53:16 +0100

    Add assortments for units

--- src/main/java/net/http/aeon/io/RecordFileReader.java
+++ src/main/java/net/http/aeon/io/RecordFileReader.java
@@ -19,13 +19,13 @@ public final class RecordFileReader extends DistanceElement {
     public RecordFileReader(Object object) {
         var lines = Files.readAllLines(AeonPathFinder.find(object)).stream().map(String::trim).filter(it -> !(it.isEmpty() || it.startsWith("#"))).toList();
         for (var index = 0; index < lines.size(); index++) {
-            index += readElement(lines.subList(index, lines.size()), objectAssortment);
+            index += readElement(lines.subList(index, lines.size()), objectAssortment, null);
         }
     }
 
-    public int readElement(List<String> lines, ObjectAssortment assortment) {
-        if (lines.get(0).contains(": [")) {
-            return readAssortment(lines.subList(1, lines.size()), assortment);
+    public int readElement(List<String> lines, ObjectAssortment assortment, String key) {
+        if (lines.get(0).contains(": [") || key == null) {
+            return readAssortment(lines.subList(1, lines.size()), assortment, lines.get(0).split(": ")[0]);
         } else if (lines.get(0).contains(": ")) {
             return readPrimitive(assortment, lines.get(1).split(": "));
         } else throw new UnsupportedWayException();
@@ -36,14 +36,21 @@ public final class RecordFileReader extends DistanceElement {
         return 1;
     }
 
-    public int readAssortment(List<String> lines, ObjectAssortment assortment) {
+    public int readAssortment(List<String> lines, ObjectAssortment assortment, String key) {
         var id = 0;
-        for (var key : assortment.getUnits().keySet()) {
+        ObjectAssortment instance = new ObjectAssortment();
 
+        for (String line : lines) {
+
+        }
+
+        for (var item : assortment.getUnits().keySet()) {
+            id += this.readElement(lines.subList(id, lines.size()), instance, item);
         }
         if (lines.get(id).contains("]")) {
             System.out.println("polo");
         }
+        assortment.append(key, instance);
         return ++id;
     }
 }
