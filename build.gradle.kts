7f303ec4bb813176a5429c84642f864d12d19817
Mirco Lindenau <mirco.lindenau@gmx.de>
2023-02-07 22:48:46 +0100

    Update version

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 allprojects {
     group = "net.http.aeon"
-    version = "1.0.4"
+    version = "1.0.5"
     description = "Configuration Framework"
 }
 
@@ -40,7 +40,7 @@ publishing {
         create("maven_public", MavenPublication::class) {
             groupId = "net.http.aeon"
             artifactId = "Aeon"
-            version = "1.0.4"
+            version = "1.0.5"
             from(components.getByName("java"))
         }
     }
