4ffe3adda7066a36e32ab8d9cd00e6e05e2488a6
Mirco Lindenau <mirco.lindenau@gmx.de>
2023-02-14 10:44:06 +0100

    Implement: Interface type reader

--- src/main/java/net/http/aeon/adapter/TypeAdapterPool.java
+++ src/main/java/net/http/aeon/adapter/TypeAdapterPool.java
@@ -11,11 +11,21 @@ public final class TypeAdapterPool {
     public <T> Optional<TypeAdapter<T>> findIf(Class<T> clazz) {
         return typeAdapters.stream().filter(it -> {
             Class<?> type = (Class<?>) ((ParameterizedType) it.getClass().getGenericSuperclass()).getActualTypeArguments()[0];
-            return type.equals(clazz);
+            return type.equals(clazz) || findAllInterfaces(clazz).contains(type);
         }).map(typeAdapter -> (TypeAdapter<T>) typeAdapter).findFirst();
     }
 
     public void registerTypeAdapter(TypeAdapter<?> typeAdapter) {
         this.typeAdapters.add(typeAdapter);
     }
+
+    public List<Class<?>> findAllInterfaces(Class<?> clazz) {
+        var interfaces = new ArrayList<Class<?>>();
+        for (var aClass : clazz.getInterfaces()) {
+            interfaces.add(aClass);
+            interfaces.addAll(findAllInterfaces(aClass));
+        }
+        return interfaces;
+    }
+
 }
