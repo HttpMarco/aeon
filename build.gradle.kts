ef156e8494cc2a77e8c9c09909ebffefc7583a5f
HttxMarco <mircolindenau04@gmail.com>
2024-01-16 14:32:14 +0100

    Add central deploy methods

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,9 +1,12 @@
 plugins {
-    id("java")
-    id("maven-publish")
+    id("java-library")
+    id("io.github.gradle-nexus.publish-plugin") version "2.0.0-rc-1"
 }
 
-group = "net.http.aeon"
+apply(plugin = "signing")
+apply(plugin = "maven-publish")
+
+group = "dev.httpmarco"
 version = "1.2.1"
 
 dependencies {
@@ -20,6 +23,50 @@ tasks.getByName<Test>("test") {
     useJUnitPlatform()
 }
 
+tasks.register<org.gradle.jvm.tasks.Jar>("javadocJar") {
+    archiveClassifier.set("javadoc")
+    from(tasks.getByName("javadoc"))
+}
+
+tasks.register<org.gradle.jvm.tasks.Jar>("sourcesJar") {
+    archiveClassifier.set("sources")
+    from(project.the<SourceSetContainer>()["main"].allJava)
+}
+
+extensions.configure<PublishingExtension> {
+    publications.apply {
+        create("maven", MavenPublication::class.java).apply {
+            from(components.getByName("java"))
+
+            artifact(tasks.getByName("sourcesJar"))
+            artifact(tasks.getByName("javadocJar"))
+
+            pom.apply {
+                name.set(project.name)
+                url.set("https://github.com/HttpMarco/aeonn")
+                description.set("A de-/serialization library to manage simple configurations")
+
+                developers {}
+
+                licenses {}
+            }
+        }
+    }
+}
+
 tasks.withType<JavaCompile> {
     options.encoding = "UTF-8"
+}
+
+nexusPublishing {
+    repositories {
+        sonatype {
+            nexusUrl.set(uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/"))
+            snapshotRepositoryUrl.set(uri("https://s01.oss.sonatype.org/content/repositories/snapshots/"))
+
+            username.set(System.getenv("MAVEN_CENTRAL_CRADINATES_USERNAME"))
+            password.set(System.getenv("MAVEN_CENTRAL_CRADINATES_PASSWORD"))
+        }
+    }
+    useStaging.set(!project.version.toString().endsWith("-SNAPSHOT"))
 }
