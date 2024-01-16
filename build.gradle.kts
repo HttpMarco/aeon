0d88b559556e9a9a5550a4c1f9995fe67a1752a7
HttxMarco <mircolindenau04@gmail.com>
2024-01-16 12:33:19 +0100

    Remove old staff and clean gradle properties

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,45 +3,23 @@ plugins {
     id("maven-publish")
 }
 
-allprojects {
-    group = "net.http.aeon"
-    version = "1.2.1"
-    description = "Configuration Framework"
-}
-
-repositories {
-    mavenCentral()
-}
+group = "net.http.aeon"
+version = "1.2.1"
 
 dependencies {
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.10.1")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.10.1")
-    compileOnly("org.projectlombok:lombok:1.18.30")
-    annotationProcessor("org.projectlombok:lombok:1.18.30")
-    testCompileOnly("org.projectlombok:lombok:1.18.30")
-    testAnnotationProcessor("org.projectlombok:lombok:1.18.30")
+    testImplementation(libs.jUnit)
+    testRuntimeOnly(libs.jUnit)
+
+    compileOnly(libs.lombok)
+    annotationProcessor(libs.lombok)
+    testCompileOnly(libs.lombok)
+    testAnnotationProcessor(libs.lombok)
 }
 
 tasks.getByName<Test>("test") {
     useJUnitPlatform()
 }
 
-tasks {
-    compileJava {
-        options.encoding = "UTF-8"
-    }
-    compileTestJava {
-        options.encoding = "UTF-8"
-    }
-}
-
-publishing {
-    publications {
-        create("maven_public", MavenPublication::class) {
-            groupId = "net.http.aeon"
-            artifactId = "Aeon"
-            version = "1.2.1"
-            from(components.getByName("java"))
-        }
-    }
+tasks.withType<JavaCompile> {
+    options.encoding = "UTF-8"
 }
